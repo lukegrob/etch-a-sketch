070859854c8a338085ae28537dadc0d125f520d7
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-10 15:03:40 +0100

    Add Grid Build Functionality

---
+++ etch-a-sketch.js
@@ -0,0 +1,21 @@
+const container = document.querySelector("#container");
+
+//possible solution:
+// set cell-size to... 1/cells?
+
+function buildGrid(cellNo)
+{
+    let totalDivs = cellNo**2;
+    let sizePer = 100/cellNo;
+    for(let i = 0;i<totalDivs;i++)
+    {
+        const divCell = document.createElement("div");
+        divCell.classList.add("cell");
+        // divCell.style.border = "1px solid red";
+        divCell.style.width = `${sizePer}%`;
+        divCell.style.height = `${sizePer}%`;
+        container.appendChild(divCell);
+    }
+}
+
+buildGrid(10);
