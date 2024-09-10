17fc6b85f10c8a4e6915086f897439664913c466
lukegrob <178822399+lukegrob@users.noreply.github.com>
2024-09-10 15:59:51 +0100

    Rebuild Grid on prompt and opacity increment

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,54 @@
 const container = document.querySelector("#container");
-
-//possible solution:
-// set cell-size to... 1/cells?
+const buttonSize = document.querySelector("#gridSize");
 
 function buildGrid(cellNo)
 {
+    //tear down any existing grid here
+    const divsToKill = document.querySelectorAll("div.cell");
+    if(divsToKill)
+    {
+        divsToKill.forEach(e => e.remove());
+    }
+
+    //
     let totalDivs = cellNo**2;
     let sizePer = 100/cellNo;
     for(let i = 0;i<totalDivs;i++)
     {
         const divCell = document.createElement("div");
         divCell.classList.add("cell");
-        // divCell.style.border = "1px solid red";
         divCell.style.width = `${sizePer}%`;
         divCell.style.height = `${sizePer}%`;
 
         divCell.addEventListener("mouseenter", (event) => {
-            event.target.style.backgroundColor = "pink";
+            event.target.style.backgroundColor = "#ff69b4";
+            event.target.style.opacity -= "-0.1";
         });
 
         container.appendChild(divCell);
     }
 }
+
+buttonSize.addEventListener("click", (result) =>
+    {   
+        let promptMessage = 'Please enter a grid size between 1-100';
+        let userInput = NaN;
+        do{
+            
+            userInput = prompt(promptMessage);
+            userInput = parseInt(userInput, 10);
+            console.log("Result: " + userInput);
+
+                if((userInput > 0) && (userInput <= 100))
+                {
+                    buildGrid(userInput);
+                }
+                else{
+                    userInput = false;
+                    promptMessage = 'Invalid Option. Please enter a grid size between 1-100';
+                }
+        }
+        while(userInput === false);
+    });
 
 buildGrid(10);
